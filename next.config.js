c95868a14ba1a0e9a64e04e8132a557793e97ce6
Victor Alves <78083141+victorgsa1@users.noreply.github.com>
2024-03-14 19:18:57 -0300

    correct

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: "export", trailingSlash: true}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
